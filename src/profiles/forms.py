6b07410c745abb0b0e4c88e4a1cdfd2e5c4c9942
tony <Tony@tappits-mbp.lan>
2020-05-20 23:01:42 +0100

    td : refactored code added docstring for functions

--- src/profiles/forms.py
+++ src/profiles/forms.py
@@ -1,21 +1,24 @@
 from django import forms
 
-
 from .models import UserAccount
 
 class UserAccountForm(forms.ModelForm):
+    """Model form for registering new users. Attrs in the widget are for placeholders."""
     first_name = forms.CharField(max_length=100,
-                           widget= forms.TextInput
-                           (attrs={'placeholder':'FIRST NAME'}))
+                                 widget=forms.TextInput
+                                 (attrs={'placeholder':'FIRST NAME'}))
     last_name = forms.CharField(max_length=100,
-                           widget= forms.TextInput
-                           (attrs={'placeholder':'LAST NAME'}))
-    account_email=forms.EmailField(max_length=256,
-                           widget= forms.TextInput
-                           (attrs={'placeholder':'EMAIL'}))
-    password = forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder':'PASSWORD'})
-                           )
-    confirm_password=forms.CharField(max_length=128, widget=forms.PasswordInput(attrs={'placeholder':'CONFIRM PASWORD'}))
+                                widget=forms.TextInput
+                                (attrs={'placeholder':'LAST NAME'}))
+    account_email = forms.EmailField(max_length=256,
+                                widget=forms.TextInput
+                                (attrs={'placeholder':'EMAIL'}))
+    password = forms.CharField(max_length=128, 
+                                widget=forms.PasswordInput
+                                (attrs={'placeholder':'PASSWORD'}))
+    confirm_password = forms.CharField(max_length=128, 
+                                widget=forms.PasswordInput
+                                (attrs={'placeholder':'CONFIRM PASWORD'}))
     class Meta:
         model = UserAccount
         fields = [
@@ -26,13 +29,22 @@ class UserAccountForm(forms.ModelForm):
             'password'
         ]
 
-    #check account does not exist already - throw validation error and handle it
+    def clean_email(self):
+        """checks user does not already exist in db"""
+        account_email = self.cleaned_data.get('account_email')
 
-    def clean_password(self, *args, **kwargs):
+        try:
+            UserAccount.objects.get(account_email=account_email)
+        except UserAccount.DoesNotExist:
+            return account_email
+
+        raise forms.ValidationError('An account with this email already exists.')
+
+    def clean_password(self):
+        """checks users passwords match in form and renders error if not"""
         password = self.cleaned_data.get("password")
         confirm_password = self.cleaned_data.get("confirm_password")
-
         if password != confirm_password:
-            raise forms.ValidationError("Passwords must match.", code=1)
-        else:
-            return password
+            raise forms.ValidationError("Passwords must match.")
+
+        return password
